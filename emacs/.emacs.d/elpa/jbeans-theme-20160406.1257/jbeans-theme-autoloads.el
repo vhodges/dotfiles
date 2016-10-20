b61355ab4b8a8ae2eafd6a5b2924c4f32fd84b6b
Vince Hodges <vhodges@gmail.com>
2016-10-20 16:12:39 -0700

    Added themes

---
+++ emacs/.emacs.d/elpa/jbeans-theme-20160406.1257/jbeans-theme-autoloads.el
@@ -0,0 +1,19 @@
+;;; jbeans-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "jbeans-theme" "jbeans-theme.el" (22522 50079
+;;;;;;  366785 237000))
+;;; Generated autoloads from jbeans-theme.el
+
+(when load-file-name (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; jbeans-theme-autoloads.el ends here
---
+++ emacs/.emacs.d/elpa/jbeans-theme-20160406.1257/jbeans-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "jbeans-theme" "20160406.1257" "Jbeans theme for GNU Emacs 24 (deftheme)" '((emacs "24")) :url "https://github.com/synic/jbeans-emacs")
