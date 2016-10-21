e9330ba0d6e5e3643c56dd985e107e4203af0808
Vince Hodges <vhodges@gmail.com>
2016-10-21 14:31:47 -0700

    Lemon bar config mostly

--- openbox/.config/openbox/autostart.sh
+++ openbox/.config/openbox/autostart.sh
@@ -1,5 +1,3 @@
 sh ~/.fehbg &
 
-i3status | dzen2 -fg white -ta r -w 1920 -fn "-misc-fixed-medium-r-normal--11-120-75-75-C-70-iso8859-1" &
-
-urxvt -geometry 130x60+494+51 &
+~/bin/panel.sh | lemonbar -p -g x25 -f "FontAwesome:size=9" -f "PragmataPro:size=9" -B#151515 | /bin/sh &
