8899657bee8c82e9704f4d6f9226c797f79bcb4f
Vince Hodges <vhodges@gmail.com>
2016-10-30 07:04:28 -0700

    Bumped status bar font size up 1

--- openbox/.config/openbox/autostart.sh
+++ openbox/.config/openbox/autostart.sh
@@ -1,3 +1,3 @@
 sh ~/.fehbg &
 
-flexstatus | lemonbar -p -g x25 -f "FontAwesome:size=9" -f "PragmataPro:size=9" -B#181835 | /bin/sh &
+flexstatus | lemonbar -p -g x25 -f "FontAwesome:size=10" -f "PragmataPro:size=10" -B#181835 | /bin/sh &
