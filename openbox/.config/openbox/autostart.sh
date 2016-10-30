757b32c8bed7d9c99cff72e5e14a7ff38202a796
Vince Hodges <vhodges@gmail.com>
2016-10-30 06:57:20 -0700

    Added statusbar configs

--- openbox/.config/openbox/autostart.sh
+++ openbox/.config/openbox/autostart.sh
@@ -1,3 +1,3 @@
 sh ~/.fehbg &
 
-~/bin/panel.sh | lemonbar -p -g x25 -f "FontAwesome:size=9" -f "PragmataPro:size=9" -B#151515 | /bin/sh &
+flexstatus | lemonbar -p -g x25 -f "FontAwesome:size=9" -f "PragmataPro:size=9" -B#181835 | /bin/sh &
